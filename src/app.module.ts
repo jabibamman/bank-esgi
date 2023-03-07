0e3aa18882b8e55c406310298f29cde1ca096d0a
James ABIB <james.bidule@gmail.com>
2023-03-07 14:52:33 +0100

    database

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,22 @@ import { AppService } from './app.service';
 import { UserController } from './user/user/user.controller';
 import { UserService } from './user/user/user.service';
 import { UserModule } from './user/user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    UserModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'test',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController, UserController],
   providers: [AppService, UserService],
 })
