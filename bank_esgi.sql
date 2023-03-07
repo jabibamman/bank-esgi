edca582e2e927d7f8f20b8abab6d1d8d7e795f68
James ABIB <james.bidule@gmail.com>
2023-03-07 16:39:18 +0100

    .sql file

--- bank_esgi.sql
+++ bank_esgi.sql
@@ -1,98 +1,54 @@
--- phpMyAdmin SQL Dump
--- version 5.1.1
--- https://www.phpmyadmin.net/
---
--- Hôte : 127.0.0.1
--- Généré le : mar. 07 mars 2023 à 15:17
--- Version du serveur : 10.4.21-MariaDB
--- Version de PHP : 8.0.11
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
+-- --------------------------------------------------------
+-- Hôte:                         127.0.0.1
+-- Version du serveur:           8.0.31 - MySQL Community Server - GPL
+-- SE du serveur:                Win64
+-- HeidiSQL Version:             11.3.0.6295
+-- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
---
--- Base de données : `bank_esgi`
---
 
--- --------------------------------------------------------
+-- Listage de la structure de la base pour bank_esgi
+CREATE DATABASE IF NOT EXISTS `bank_esgi` /*!40100 DEFAULT CHARACTER SET latin1 */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `bank_esgi`;
 
---
--- Structure de la table `bankaccount`
---
+-- Listage de la structure de la table bank_esgi. bankaccount
+CREATE TABLE IF NOT EXISTS `bankaccount` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `solde` double NOT NULL,
+  `userId` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_user_bankaccount` (`userId`),
+  CONSTRAINT `fk_user_bankaccount` FOREIGN KEY (`userId`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE `bankaccount` (
-  `id` int(11) NOT NULL,
-  `solde` double NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+-- Listage des données de la table bank_esgi.bankaccount : ~1 rows (environ)
+DELETE FROM `bankaccount`;
+/*!40000 ALTER TABLE `bankaccount` DISABLE KEYS */;
+INSERT INTO `bankaccount` (`id`, `solde`, `userId`) VALUES
+	(1, 5000, 1);
+/*!40000 ALTER TABLE `bankaccount` ENABLE KEYS */;
 
--- --------------------------------------------------------
+-- Listage de la structure de la table bank_esgi. user
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
---
--- Structure de la table `user`
---
+-- Listage des données de la table bank_esgi.user : ~1 rows (environ)
+DELETE FROM `user`;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` (`id`, `name`) VALUES
+	(1, 'James');
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
 
-CREATE TABLE `user` (
-  `id` int(11) NOT NULL,
-  `name` varchar(30) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- Structure de la table `user_bankaccount`
---
-
-CREATE TABLE `user_bankaccount` (
-  `idUser` int(11) NOT NULL,
-  `idBankAccount` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Index pour les tables déchargées
---
-
---
--- Index pour la table `bankaccount`
---
-ALTER TABLE `bankaccount`
-  ADD PRIMARY KEY (`id`);
-
---
--- Index pour la table `user`
---
-ALTER TABLE `user`
-  ADD PRIMARY KEY (`id`);
-
---
--- Index pour la table `user_bankaccount`
---
-ALTER TABLE `user_bankaccount`
-  ADD PRIMARY KEY (`idUser`,`idBankAccount`);
-
---
--- AUTO_INCREMENT pour les tables déchargées
---
-
---
--- AUTO_INCREMENT pour la table `bankaccount`
---
-ALTER TABLE `bankaccount`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT pour la table `user`
---
-ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-COMMIT;
-
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
